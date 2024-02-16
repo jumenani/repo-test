e0bba9c3903c58411b3f3bec2637fb780b278a34
Juliano <jumenani@gmail.com>
2024-02-16 11:52:43 -0300

    Meu primeiro arquivo Commitado

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Jornada dos Dados")
+
+# Variaveis
+idade = 49
+nome = "Juliano"
+
+# Print
+print(idade)
+print(nome)
+
+# Soma
+nova_soma = idade + 1
+print(nova_soma)
+
+# Lista
+lista = [1,2,3,"Juliano",True]
+print(lista)
+
+# Metodos
+lista_invertida = lista.reverse()
+print(lista_invertida)
